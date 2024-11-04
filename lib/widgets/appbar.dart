872afdd64c3d10b4e6f74b002ad98b74503ab7eb
kaljitism <dwikaljit@gmail.com>
2024-11-04 06:45:38 +0530

    commit

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
-
-class ChatterDashAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const ChatterDashAppBar({super.key});
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: const Text('ChatterDash'),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class ChatterDashAppBar extends StatelessWidget implements PreferredSizeWidget {
+//   const ChatterDashAppBar({super.key});
+//
+//   @override
+//   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       title: const Text('ChatterDash'),
+//     );
+//   }
+// }
